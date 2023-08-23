062ea2b67b12dbc13d46a4930e74510828ad4ab0
Anthony P. <43166263+anthonypz@users.noreply.github.com>
2023-08-22 23:24:58 -0700

    fix: background color and padding

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -58,7 +58,9 @@ const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
 					<Library songs={songs} />
 				</Box>
 			</div>
-			<main className='h-full flex-1 overflow-y-auto py-2'>{children}</main>
+			<main className='h-full flex-1 overflow-y-auto py-2 bg-black md:pr-2'>
+				{children}
+			</main>
 		</div>
 	);
 };
